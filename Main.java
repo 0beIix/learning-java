b9a6ac16b6e395240ede0765fa178aa8fc37f9d8
Obelix <obelixdergrose@gmail.com>
2023-08-08 12:05:53 -0400

    #

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
-        int age = 23;
-        String message = "test";
-        System.out.println(message.toLowerCase() +" "+ age);
+        int total = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("People contributing:");
+        int contributing = scanner.nextByte();
+        int[] costs = new int [contributing];
+        System.out.print("People participating:");
+        int participating = scanner.nextByte();
+        for (int i = 0; i < contributing; i++){
+            System.out.print("Enter price payed be person " + (i+1) + ": ");
+            int price = scanner.nextByte();
+            costs[i] = price;
+            total += price;
+            System.out.println(total);
+        }
+        int totalCost = total/participating;
+        System.out.println(totalCost);
+        for (int i = 0; i < contributing; i++){
+            System.out.println("Person " + (i+1) + " payed " + costs[i]);
+        }
     }
 }
