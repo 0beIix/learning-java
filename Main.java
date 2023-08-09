725fad59ee3633489142bd62867ed894ba0c37f7
Obelix <obelixdergrose@gmail.com>
2023-08-09 09:45:19 -0400

    update

--- Main.java
+++ Main.java
@@ -2,24 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-        int total = 0;
+        float total = 0;
+        float nonContTotal = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("People contributing:");
+        System.out.print("People contributing: ");
         int contributing = scanner.nextByte();
-        int[] costs = new int [contributing];
-        System.out.print("People participating:");
+        float[] costs = new float [contributing];
+        float[] owed = new float [contributing];
+        System.out.print("People participating: ");
         int participating = scanner.nextByte();
+        int notContributing = participating - contributing;
+        //cycle through contributers to colect prices 
         for (int i = 0; i < contributing; i++){
             System.out.print("Enter price payed be person " + (i+1) + ": ");
-            int price = scanner.nextByte();
+            float price = scanner.nextFloat();
             costs[i] = price;
             total += price;
-            System.out.println(total);
         }
-        int totalCost = total/participating;
-        System.out.println(totalCost);
+        System.out.println("\nTotal cost: " + total);
+        System.out.println("\n\n\t Cost organizer - owed \n");
+        float totalSharedCost = total/participating;
         for (int i = 0; i < contributing; i++){
-            System.out.println("Person " + (i+1) + " payed " + costs[i]);
+            System.out.print("Person " + (i+1) + " payed " + costs[i]);
+            owed[i] = costs[i] - totalSharedCost;
+            System.out.println(" is owed " + owed[i]);
         }
+        System.out.println("\n\nShared cost per person: " + totalSharedCost);
+        nonContTotal = totalSharedCost * notContributing;
+        System.out.println("Total non contributing: " + nonContTotal);
+        System.out.println("Each non contributer pays person 1: " + totalSharedCost);
+        System.out.println("This adds up to: " + nonContTotal);
+        System.out.println("\nP1 pays P2: " + owed[1]);
+        nonContTotal -= owed[1];
+        System.out.println("P1 pays P3: " + owed[2]);
+        nonContTotal -= owed[2];
+        System.out.println("This leaves P1 with: " + nonContTotal);
+        scanner.close();
     }
 }
