6ba71337ff92cb1e5056f30e163ac9661a529ad8
Obelix <obelixdergrose@gmail.com>
2023-08-11 10:54:14 -0400

    "update - working properly"

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String args[]){
         float total = 0;
         float nonContTotal = 0;
+        int mostOwed = 0;
+        float owedAmount = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.print("People contributing: ");
         int contributing = scanner.nextByte();
@@ -26,17 +28,25 @@ public class Main {
             System.out.print("Person " + (i+1) + " payed " + costs[i]);
             owed[i] = costs[i] - totalSharedCost;
             System.out.println(" is owed " + owed[i]);
+            if(owed[i] > owedAmount){ 
+                owedAmount = owed[i];
+                mostOwed = i;
+            }
         }
+        System.out.println("Highest amount: " + owedAmount + " is owed to contributer: " + ((int)mostOwed+1));
         System.out.println("\n\nShared cost per person: " + totalSharedCost);
         nonContTotal = totalSharedCost * notContributing;
-        System.out.println("Total non contributing: " + nonContTotal);
-        System.out.println("Each non contributer pays person 1: " + totalSharedCost);
+        System.out.println("Each non contributer pays person " + (mostOwed+1) + ": " + totalSharedCost);
         System.out.println("This adds up to: " + nonContTotal);
-        System.out.println("\nP1 pays P2: " + owed[1]);
-        nonContTotal -= owed[1];
-        System.out.println("P1 pays P3: " + owed[2]);
-        nonContTotal -= owed[2];
-        System.out.println("This leaves P1 with: " + nonContTotal);
+        for (int i = 0; i < contributing; i++){
+            if (i == mostOwed){}
+            else {
+                System.out.println("P" + (mostOwed+1) + " pays P" + (i+1) + ": " + owed[i]);
+                nonContTotal -= owed[i];
+            }
+
+        }
+        System.out.println("P " + (mostOwed+1) + " keeps: " + nonContTotal);
         scanner.close();
     }
 }
